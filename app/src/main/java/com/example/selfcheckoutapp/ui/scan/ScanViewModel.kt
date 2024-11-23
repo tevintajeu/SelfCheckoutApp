8ee5a52dffc331f880d5d1184cbad0c5b805277b
Maiyo-D-D <dennis.kiprop@strathmore.edu>
2024-11-24 00:40:33 +0300

    complete baseline

---
+++ app/src/main/java/com/example/selfcheckoutapp/ui/scan/ScanViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.selfcheckoutapp.ui.scan
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ScanViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is scan Fragment"
+    }
+    val text: LiveData<String> = _text
+}
